1c529c2bb5f446551eac49bcab31960f444769b0
Cesar <esapinosamatoscesar131@gmail.com>
2026-07-09 11:23:53 -0400

    Agregando interfaces base y específicas para repositorios de clientes, habitaciones y reservas

---
+++ HotelCrownePlaza/SistemaHotelCrowne.Application/Interfaces/IHabitacionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SistemaHotelCrowne.Domain.Entities;
+using SistemaHotelCrowne.Domain.Enums;
+
+namespace SistemaHotelCrowne.Application.Interfaces
+{
+    public interface IHabitacionRepository : IRepositoryBase<Habitacion>
+    {
+        // Permite buscar habitaciones según su estado actual (Disponible, Ocupada, Mantenimiento)
+        Task<IReadOnlyList<Habitacion>> GetByEstadoAsync(EstadoHabitacion estado);
+    }
+}
---
+++ HotelCrownePlaza/SistemaHotelCrowne.Application/Interfaces/IReservacionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SistemaHotelCrowne.Domain.Entities;
+
+namespace SistemaHotelCrowne.Application.Interfaces
+{
+    public interface IReservacionRepository : IRepositoryBase<Reservacion>
+    {
+        // Obtiene todas las reservaciones que ha hecho un cliente en su historial
+        Task<IReadOnlyList<Reservacion>> GetByClienteIdAsync(int clienteId);
+
+        // Verifica si una habitación específica ya está ocupada en un rango de fechas
+        Task<bool> IsHabitacionOcupadaAsync(int habitacionId, System.DateTime fechaInicio, System.DateTime fechaFin);
+    }
+}
