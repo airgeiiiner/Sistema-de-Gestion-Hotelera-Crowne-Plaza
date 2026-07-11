0dd91d5d92a083c67cae1bfc90953b655732565a
¨Franyel¨ <franyelmatos750@gmail.com>
2026-07-11 19:01:04 -0400

    Agregado de excepciones claves

---
+++ HotelCrownePlaza/SistemaHotelCrowne.Domain/Exceptions/HabitacionNoDisponibleException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaHotelCrowne.Domain.Exceptions
+{
+    public class HabitacionNoDisponibleException : Exception
+    {
+        public HabitacionNoDisponibleException(string tipoHabitacion, DateTime fecha)
+            : base($"La habitación de tipo '{tipoHabitacion}' no está disponible para la fecha {fecha.ToShortDateString()}.");
+    }
+}
---
+++ HotelCrownePlaza/SistemaHotelCrowne.Domain/Exceptions/ReservacionNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaHotelCrowne.Domain.Exceptions
+{
+    internal class ReservacionNotFoundException
+    {
+        public ReservacionNotFoundException(Guid id)
+            : base($"No se encontró ninguna reservación activa con el ID: {id}.");
+    }
+}
